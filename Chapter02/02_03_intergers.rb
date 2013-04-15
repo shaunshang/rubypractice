edd8901e509a2e1453d5a017aa4f1fd6af8fc3f9
Shaun Shang <shaun.shang@tbwa.com>
2013-04-15 15:06:07 +0800

    chapter02

---
+++ Chapter02/02_03_intergers.rb
@@ -0,0 +1,11 @@
+x = 2
+a = 4 / 2
+b = 4 * 2
+c = 123456789123456789123456789123456789123456789123456789
+d = -200
+
+puts x + a + b
+puts a.class
+puts c.class
+puts b.next
+puts d.abs
---
+++ Chapter02/02_04_floats.rb
@@ -0,0 +1,10 @@
+a = 12345.6789
+b = 10
+c = a.round
+d = a.to_i
+
+puts a.class
+puts (b+1).class
+puts (b+0.1).class
+puts c.class
+puts d.class
