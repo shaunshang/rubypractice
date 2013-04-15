fdb5aeb091fc0b7611a3e7595277b9e09aa9c817
Shaun Shang <shaun.shang@tbwa.com>
2013-04-15 14:40:04 +0800

    chapter01, chapter02-02, chapter02-03

---
+++ Chapter02/02_02_variables.rb
@@ -0,0 +1,18 @@
+#Variables: scope indicators
+#   Global      $variable
+#   Class       @@variable
+#   Instance    @variable
+#   Local       variable
+#   Block       variable
+
+
+x = 1
+x = x + 2
+puts x
+
+first_variable = 4
+puts first_variable
+
+articles_written = 100
+puts articles_written
+
